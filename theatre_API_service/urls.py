83232fbc60909ff6c5e97e5f995c079f8b04855d
Petro Abramov <sebshe3@gmail.com>
2023-09-09 20:10:41 +0300

    Renamed endpoint

--- theatre_API_service/urls.py
+++ theatre_API_service/urls.py
@@ -24,7 +24,7 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/cinema/", include("theatre.urls", namespace="theatre")),
+    path("api/theatre/", include("theatre.urls", namespace="theatre")),
     path("api/user/", include("users.urls", namespace="user")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
