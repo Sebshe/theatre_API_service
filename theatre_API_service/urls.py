3c7f351519c7f850e82174eb913c27accad38144
Petro Abramov <sebshe3@gmail.com>
2023-09-09 20:08:29 +0300

    Added settings to urls in project

--- theatre_API_service/urls.py
+++ theatre_API_service/urls.py
@@ -24,6 +24,8 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/cinema/", include("theatre.urls", namespace="theatre")),
+    path("api/user/", include("users.urls", namespace="user")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/doc/swagger/",
